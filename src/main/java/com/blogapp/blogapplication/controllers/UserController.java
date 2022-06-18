de1729a67ae05e3db8b8c4eda5a262b2c795021b
JigarJoshi <jigar.pj@somaiya.edu>
2022-06-19 01:24:37 +0530

    fix: login request changed form get to post

--- src/main/java/com/blogapp/blogapplication/controllers/UserController.java
+++ src/main/java/com/blogapp/blogapplication/controllers/UserController.java
@@ -4,7 +4,6 @@ import com.blogapp.blogapplication.dto.UserLoginDto;
 import com.blogapp.blogapplication.dto.UserRegistrationDto;
 import com.blogapp.blogapplication.models.UserEntity;
 import com.blogapp.blogapplication.services.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,47 +19,49 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @CrossOrigin(
   // Access-Control-Allow-Origin
   origins = { "*" },
-  
-  // Alternative to origins that supports more flexible originpatterns. 
+  // Alternative to origins that supports more flexible originpatterns.
   // Please, see CorsConfiguration.setAllowedOriginPatterns(List)for details.
-  // originPatterns = { "" },   
-  
+  // originPatterns = { "" },
+
   // Access-Control-Allow-Credentials
   allowCredentials = "false",
-  
   // Access-Control-Allow-Headers
   allowedHeaders = { "*" },
-  
   // Access-Control-Expose-Headers
   exposedHeaders = { "*" }
-  
 )
 @RestController
 @RequestMapping("/user")
 public class UserController {
   @Autowired
   private UserService userService;
-  
+
   @PostMapping("/register")
   public ResponseEntity<?> registerUserAccount(
-      @RequestBody UserRegistrationDto userRegistrationDto) {
+    @RequestBody UserRegistrationDto userRegistrationDto
+  ) {
     //add check if username already exists in db
 
     if (userService.existsByUsername(userRegistrationDto)) {
-      return new ResponseEntity<>("Username already taken!",HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(
+        "Username already taken!",
+        HttpStatus.BAD_REQUEST
+      );
     } else if (userService.existsByEmail(userRegistrationDto)) {
-      return new ResponseEntity<>("Email is already registered!", HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(
+        "Email is already registered!",
+        HttpStatus.BAD_REQUEST
+      );
     }
     //add check if email already exists in db
     userService.save(userRegistrationDto);
-    return new ResponseEntity<>("User Registered Sucessfully",HttpStatus.OK);
+    return new ResponseEntity<>("User Registered Sucessfully", HttpStatus.OK);
   }
 
-  @GetMapping("/login")
+  @PostMapping("/login")
   public ResponseEntity<?> loginUser(@RequestBody UserLoginDto userLoginDto) {
     UserEntity user = null;
     if (userService.findByUsername(userLoginDto) != null) {
@@ -68,12 +69,17 @@ public class UserController {
     } else if (userService.findByEmail(userLoginDto) != null) {
       user = userService.findByEmail(userLoginDto);
     } else {
-      return new ResponseEntity<>("Incorrect username or email", HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(
+        "Incorrect username or email",
+        HttpStatus.BAD_REQUEST
+      );
     }
     if (!userService.validatePassword(user, userLoginDto)) {
-      return new ResponseEntity<>("Incorrect Credentials!", HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(
+        "Incorrect Credentials!",
+        HttpStatus.BAD_REQUEST
+      );
     }
     return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
   }
-  
 }
